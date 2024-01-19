35d0314fd58eb9a5916f778c62d00419d33870b5
Siqust <Alexasey2009@mail.ru>
2024-01-19 19:00:42 +0300

    third commit

--- Assets/scripts/Player/CheckGround.cs
+++ Assets/scripts/Player/CheckGround.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,28 +21,3 @@ public class CheckGround : MonoBehaviour
         }
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CheckGround : MonoBehaviour
-{
-    public bool grounded;
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag != "Player")
-        {
-            grounded = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag != "Player")
-        {
-            grounded = false;
-        }
-    }
-}
->>>>>>> 0cc5975fc469f8d25740aa00d17b5213b2cda509
--- Assets/scripts/Player/Movement.cs
+++ Assets/scripts/Player/Movement.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,31 +18,6 @@ public class Movement : MonoBehaviour
     void Update()
     {
         xmove = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xmove* speed * Time.deltaTime, 0);
+        rb.velocity = new Vector2(xmove * speed * Time.deltaTime, 0);
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Movement : MonoBehaviour
-{
-    private Rigidbody2D rb;
-    public float speed = 0.5f;
-    private float xmove;
-    public float jump_force;
-    public CheckGround checkGround;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        xmove = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xmove* speed * Time.deltaTime, 0);
-    }
-}
->>>>>>> 0cc5975fc469f8d25740aa00d17b5213b2cda509
